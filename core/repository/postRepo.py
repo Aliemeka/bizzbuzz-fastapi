e49ff15cbecd4af359c9f0d7899ed410a823ee80
Emeka Allison <emekaallison4@gmail.com>
2021-09-14 07:29:44 +0100

    Added put and patch method routes

--- core/repository/postRepo.py
+++ core/repository/postRepo.py
@@ -1,9 +1,19 @@
 from typing import List
 
-from ..schemas.postSchema import BasePost, Post
+from ..schemas.postSchema import BasePost, Post, Status
 
 
 posts: List[Post] = []
+
+
+def generate_number():
+    i = 1
+    while True:
+        yield i
+        i += 1
+
+
+post_id = generate_number()
 
 
 def get_posts():
@@ -12,7 +22,7 @@ def get_posts():
 
 def create_post(post: BasePost):
     somePost = dict(post)
-    somePost["id"] = len(posts) + 1
+    somePost["id"] = next(post_id)
     newPost = Post(**somePost)
     posts.append(newPost)
     return newPost
@@ -25,5 +35,40 @@ def add_posts(posts: List[BasePost]):
 
 def add_mulitple(postList: List[BasePost]):
     posts = [post for post in add_posts(postList)]
-    print(posts)
     return posts
+
+
+def get_post(id: int):
+    if len(posts) < 1:
+        raise Exception({"message": "There are no posts yet!"})
+    for post in posts:
+        if post.id == id:
+            return post
+
+    raise Exception({"message": f"Cannot find post with id: {id}"})
+
+
+def get_post_by_status(status: Status, postList: List[Post]):
+    return [post for post in postList if post.status == status]
+
+
+def get_post_by_attribute(attribute: str, postList: List[Post]):
+    return [
+        post
+        for post in postList
+        if attribute.lower() in post.name.lower()
+        or attribute.lower() in post.description.lower()
+    ]
+
+
+def edit_post(id: int, details: BasePost):
+    post = get_post(id)
+    post.name = details.name
+    post.description = details.description
+    return post
+
+
+def change_post_status(id: int, status: Status):
+    post = get_post(id)
+    post.status = status
+    return post
--- core/routers/posts.py
+++ core/routers/posts.py
@@ -1,14 +1,21 @@
-from fastapi import APIRouter
-from typing import List
-from ..schemas.postSchema import Post, BasePost
+from fastapi import APIRouter, HTTPException
+from typing import List, Optional, TypedDict
+from ..schemas.postSchema import Post, BasePost, Status
 from ..repository import postRepo
 
 router = APIRouter(prefix="/posts", tags=["posts"])
 
+StatusPayload = TypedDict("StatusPayload", status=Status)
+
 
 @router.get("/", response_model=List[Post])
-async def get_posts():
-    return postRepo.get_posts()
+async def get_posts(status: Optional[Status] = None, search: Optional[str] = None):
+    posts = postRepo.get_posts()
+    if status != None:
+        posts = postRepo.get_post_by_status(status, posts)
+    if search:
+        posts = postRepo.get_post_by_attribute(search, posts)
+    return posts
 
 
 @router.post("/", status_code=201, response_model=Post)
@@ -19,3 +26,30 @@ async def create_post(post: BasePost):
 @router.post("/multiple", response_model=List[Post])
 async def add_multiple(posts: List[BasePost]):
     return postRepo.add_mulitple(posts)
+
+
+@router.get("/{id}", response_model=Post)
+async def get_post(id: int):
+    try:
+        post = postRepo.get_post(id)
+        return post
+    except Exception as e:
+        raise HTTPException(status_code=404, detail=e.args[0]["message"])
+
+
+@router.put("/{id}", response_model=Post)
+async def update_post(id: int, details: BasePost):
+    try:
+        post = postRepo.edit_post(id, details)
+        return post
+    except Exception as e:
+        raise HTTPException(status_code=404, detail=e.args[0]["message"])
+
+
+@router.patch("/{id}/status", response_model=Post)
+async def change_post_status(id: int, payload: StatusPayload):
+    try:
+        post = postRepo.change_post_status(id, payload["status"])
+        return post
+    except Exception as e:
+        raise HTTPException(status_code=404, detail=e.args[0]["message"])
