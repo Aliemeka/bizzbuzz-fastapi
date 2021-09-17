da166cb91f5b1b6d3d908681910fb8c4bffd6643
Emeka Allison <emekaallison4@gmail.com>
2021-09-17 18:41:15 +0100

    Added token type to token schema

--- core/schemas/tokenSchema.py
+++ core/schemas/tokenSchema.py
@@ -1,7 +1,9 @@
 from pydantic import BaseModel
+from typing import Optional
 
 
 class Token(BaseModel):
     token: str
     user: str
+    token_type: Optional[str] = "Bearer Token"
     is_active: bool
