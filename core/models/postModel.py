eb8d0778631e771f4f9ed2f1bd12d19a3082761c
Emeka Allison <emekaallison4@gmail.com>
2021-09-21 16:30:39 +0100

    Change author_id type from integer to guid

--- core/models/postModel.py
+++ core/models/postModel.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, Enum, ForeignKey, String, Integer
 from sqlalchemy.orm import relationship
 
 from .base import BaseModel
+from ..config.typing import GUID
 from ..config.enums import Status
 
 
@@ -9,6 +10,6 @@ class Post(BaseModel):
     title = Column(String)
     description = Column(String)
     status = Column(Enum(Status))
-    author_id = Column(Integer, ForeignKey("user.id"))
+    author_id = Column(GUID, ForeignKey("user.id"))
 
     author = relationship("User", back_populates="posts")
