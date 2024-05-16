e0433ccaca9fed613f379df3a95dc2081b26dd59
Emeka Allison <emekaallison4@gmail.com>
2024-05-16 13:02:09 +0100

    Audits to Hash class method
    Added `self` to each method in `Hash` class

--- core/utils/authentication.py
+++ core/utils/authentication.py
@@ -27,12 +27,12 @@ class InvalidTokenException(Exception):
 
 
 class Hash:
-    def hash_password(password: str) -> str:
+    def hash_password(self, password: str) -> str:
         salt = bcrypt.gensalt()
         hashed_password = bcrypt.hashpw(str.encode(password), salt)
         return hashed_password.decode()
 
-    def verify_password(plain_password: str, hashed_password: str):
+    def verify_password(self, plain_password: str, hashed_password: str):
         return bcrypt.checkpw(str.encode(plain_password), str.encode(hashed_password))
 
 
